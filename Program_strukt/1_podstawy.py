bb9d1d31c2753df7161054005f2a6c6dbe7eaea8
SergiuszKubicki <sergiuszkubicki98@gmail.com>
2020-03-29 13:41:52 +0200

    change2

--- Program_strukt/1_podstawy.py
+++ Program_strukt/1_podstawy.py
@@ -1,2 +1,39 @@
 print("1111")
 print(8)
+
+#potęgowanie
+pow = 2 ** 10
+print(pow)
+
+text = "CDV"
+print(text * 2)
+
+#pobieranie danych z klawiatury
+# + konkatenacja = polaczenie 2 stringow
+
+name = input()
+print("Twoje imie :" + name)
+
+surname=input()
+print("Twoje imie: " + name + ", nazwisko: "+ surname)
+
+length = len(surname)
+print(length)
+print(type(surname))
+print(type(length))
+
+lenght = str(length)
+print(type(length))
+
+#użytkownik podaje z klawiatury swoje dane
+#imie nazwisko wiek
+#wystwietl dane w formacseie:
+#imie i nazwisko: ..., wiek:... lat
+
+name = input()
+surname = input()
+wiek = input()
+
+print("twoje imie: " + name + ",nazwisko: " + surname + ",wiek: "+ wiek)
+
+
